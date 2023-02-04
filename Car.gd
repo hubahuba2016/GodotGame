c595f031584177934814043437dd54a117c47f4b
Hubaka Ghayati Setyakurnia <hubakag@gmail.com>
2023-02-04 10:34:24 +0700

    change delta to _delta

--- Car.gd
+++ Car.gd
@@ -1,5 +1,5 @@
 extends VehicleBody
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	steering = Input.get_axis("right","left") * 0.4
 	engine_force = Input.get_axis("back","forward") * 150
